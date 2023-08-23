f89fd57e66d91ccb764cf0ca5f1634539f4207fa
karani-ken <academice79@gmail.com>
2023-08-23 16:40:51 +0300

    second commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//basic types
+let id = 5; //numbers
+let company = "Kenneth Karani"; //strings
+let isPublished = true; //boolean
+let x = 'Hello'; //any type
+let age;
+age: 30;
+let ids = [1, 2, 3, 4, 5, 6]; //arrays
+let arr = [1, 2, "Hello", 'B'];
+//tuple
+let person = [1, "Hello", false, 2];
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+//basic types
+let id: number  = 5//numbers
+let company: string = "Kenneth Karani"//strings
+
+let isPublished: boolean = true//boolean
+
+let x: any = 'Hello'//any type
+
+let age: number
+age:30
+let ids: number[] = [1,2,3,4,5,6] //arrays
+let arr:any[]= [1,2,"Hello",'B']
+//tuple
+let person:[number, string,boolean,any]=[1,"Hello",false,2]
+
+
+
