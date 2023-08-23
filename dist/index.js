299ed63df8cb3ff894caa5cfb14d5fed90677b55
karani-ken <academice79@gmail.com>
2023-08-23 17:42:07 +0300

    third commit

--- dist/index.js
+++ dist/index.js
@@ -10,3 +10,44 @@ let ids = [1, 2, 3, 4, 5, 6]; //arrays
 let arr = [1, 2, "Hello", 'B'];
 //tuple
 let person = [1, "Hello", false, 2];
+//array tuple
+let employee;
+employee = [
+    [1, "Kelly"],
+    [2, "John"],
+    [3, "Jill"]
+];
+//Union Types
+//unions : a union formed from two or more other types, representing values that may be
+//any one of those types. Holds more than one type
+let pid = 22;
+pid = "Hello";
+//Enum
+//An enum is a special "class" that represents a group of constants (unchangeable variables)
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["North"] = 1] = "North";
+    Direction1[Direction1["South"] = 2] = "South";
+    Direction1[Direction1["West"] = 3] = "West";
+    Direction1[Direction1["East"] = 4] = "East";
+})(Direction1 || (Direction1 = {}));
+//console.log(Direction1.East)//displays 4 when 1 is assigned to North
+//Objects in Typescript
+const car = {
+    type: "Toyota",
+    model: "Corolla",
+    year: 2009
+};
+const car2 = {
+    type: "Toyota",
+    model: "Corolla",
+    year: 2009
+};
+//type Assertion
+let cid = 1;
+let customerId = cid; //implicitly defines customer id into a number and cannot be reassigned to 
+//other value
+//customerId = true will cause an error
+//alternatively you can assert the type like this....
+let CustomerID = cid;
+//Fuctions
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,60 @@ let ids: number[] = [1,2,3,4,5,6] //arrays
 let arr:any[]= [1,2,"Hello",'B']
 //tuple
 let person:[number, string,boolean,any]=[1,"Hello",false,2]
+//array tuple
+let employee:[number,string][]
+
+employee =[
+    [1,"Kelly"],
+    [2,"John"],
+    [3,"Jill"]
+]
+//Union Types
+//unions : a union formed from two or more other types, representing values that may be
+//any one of those types. Holds more than one type
+let pid: string | number =22
 
 
+pid="Hello"
 
+//Enum
+//An enum is a special "class" that represents a group of constants (unchangeable variables)
+enum Direction1{
+    North=1, //North= 'North'
+    South,
+    West,
+    East,
+}
+//console.log(Direction1.East)//displays 4 when 1 is assigned to North
+//Objects in Typescript
+const car:{
+    //assigning types
+    type:string,
+    model:string,
+    year:number
+} = {
+    type:"Toyota",
+    model:"Corolla",
+    year: 2009
+}
+//alternative method 
+type car2 = {
+    type:string,
+    model:string,
+    year:number
+}
+const car2={
+    type:"Toyota",
+    model:"Corolla",
+    year: 2009
+}
+//type Assertion
+let cid: any = 1
+let customerId = <number>cid //implicitly defines customer id into a number and cannot be reassigned to 
+//other value
+//customerId = true will cause an error
+//alternatively you can assert the type like this....
+let CustomerID = cid as number
+
+
+//Fuctions
